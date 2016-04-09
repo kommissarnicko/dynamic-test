7ce097ba540e9e333155b5b873e827caf49cd1b3
kommissarnicko <kommissarnicko@gmail.com>
2016-04-08 22:08:47 -0600

    remv requirement.txt, adjust dynamic-test to use webapp2

--- dynamic-test.py
+++ dynamic-test.py
@@ -1,10 +1,17 @@
-from flask import Flask
+import webapp2
 from datetime import datetime
 
+class DynamicTestHandler(webapp2.RequestHandler):
+	def get(self):
+		thetime = datetime.now().time()
+		greeting = "Nick Williams Site, time now is " + str(thetime)
+		self.response.write(str(greeting))
+		
+app = webapp2.WSGIApplication([
+	('/', DynamicTestHandler), 
+], debug = True)
 
-app = Flask(__name__)
-
-
+"""
 @app.route('/')
 def hello():
     """Dynamic greeting."""
@@ -15,3 +22,4 @@ def hello():
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
+"""
